56a8ad1a4556d5e592cdc80a8c643b5d7e520f1d
Liam Gulliver <128083+lgulliver@users.noreply.github.com>
2020-12-10 19:52:14 +0000

    added santa

--- api/UploadLetter.cs
+++ api/UploadLetter.cs
@@ -6,30 +6,57 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.Linq; 
+using Microsoft.Azure.Storage.Blob;  
 
 namespace Azureish.Santa.Mailroom.Api
 {
     public static class UploadLetter
     {
+        
         [FunctionName("UploadLetter")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            [Blob("letters", Connection = "StorageAccountConnectionString")] CloudBlobContainer outputContainer,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+            
+            await outputContainer.CreateIfNotExistsAsync();
 
-            string name = req.Query["name"];
+            var fileForUpload = req.Form.Files[0];            
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            if(!IsImage(fileForUpload)) {
+                log.LogError($"An error occured during upload: Invalid File Type.");
+                return new BadRequestResult();
+            }
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            var blobName = $"{Guid.NewGuid().ToString()}{fileForUpload.FileName.Substring(fileForUpload.FileName.LastIndexOf('.'))}";
+            var fileStream = fileForUpload.OpenReadStream();          
 
-            return new OkObjectResult(responseMessage);
+            var cloudBlockBlob = outputContainer.GetBlockBlobReference(blobName);
+
+            try {
+                await cloudBlockBlob.UploadFromStreamAsync(fileStream);
+            } catch (Exception ex) {
+                log.LogError($"An error occured during upload: {ex.Message}");
+                return new BadRequestResult();
+            }
+
+            return new OkObjectResult(blobName);
         }
+
+        public static bool IsImage(IFormFile file)
+        {
+            if (file.ContentType.Contains("image"))
+            {
+                return true;
+            }
+
+            string[] formats = new string[] { ".jpg", ".png", ".gif", ".jpeg" };
+
+            return formats.Any(item => file.FileName.EndsWith(item, StringComparison.OrdinalIgnoreCase));
+        }
+
     }
 }
